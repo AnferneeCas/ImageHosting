149079d30eff4c3205d3790edc7d0c1e2651a788
Anfernee Castillo <anfer@sube.la>
2020-11-14 21:08:54 -0600

    base line

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const { DefaultDeserializer } = require("v8");
 
 const app = express();
+
+// Save an image
+app.post("/image", function (req, res) {
+  //Validate image size (32mb)
+  //Create uuid
+  // Save image in local filesystem
+  // Save the image uuid and expired date in DB
+  // respond with the image url
+  // considerations
+  // Temporary image route = images/tmp
+  // User specific image route is  = images/users/username
+});
+
+app.get("/image/:imageName", function (req, res) {});
 
 app.listen(3000);
