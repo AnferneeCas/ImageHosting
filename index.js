ac3b0e43fa152966f1b1efc23d88c25a37990a1b
Hairo Santana Calderon <hairo.santana.calderon@outlook.com>
2020-11-17 16:27:05 -0400

    rout /image updated [save image in local, save in db, ]

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const os = require("os");
+const path = require("path");
 const { DefaultDeserializer } = require("v8");
 const mongoose = require("mongoose");
+const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 
 require("./schema");
@@ -14,19 +17,70 @@ mongoose.connect(
   }
 );
 
+
+
+
 const app = express();
+const dir  = path.join(__dirname, "images");
+
+/* Middlewares */
+const middlewares = [
+  express.json({ limit: "32mb" }),
+  express.static(dir),
+];
+app.use(middlewares);
+
+var mime = {
+  html: 'text/html',
+  txt: 'text/plain',
+  css: 'text/css',
+  gif: 'image/gif',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  js: 'application/javascript'
+};
 
 // Save an image
-app.post("/image", async function (req, res) {
-  //Validate image size (32mb)
-  //Create uuid
-  // Save image in local filesystem
-  // Save the image uuid and expired date in DB
+app.post("/image", async (req, res) => {
+  // Vars
+  const { file: imageBase64, user, format } = req.body;
+  var hostname = os.hostname();
   const uuid = uuidv4();
-  const Image = mongoose.model("Image");
-  var tmpImage = await Image.create({ name: uuid, expireDate: Date.now() });
-  console.log("imagen creada");
-  res.send(tmpImage);
+  let ReadableStream = require("stream").Readable;
+  const imageBufferData = Buffer.from(imageBase64, "base64");
+  let streamObjet = new ReadableStream();
+
+  let file = !user
+    ? `/images/${uuid}.${format}`
+    : /* check when i save a picture from a regitered  user  -------pending */
+      `/images/users/${uuid}.${format}`;
+
+  streamObjet.push(imageBufferData);
+  streamObjet.push(null);
+  streamObjet.pipe(fs.createWriteStream(`${__dirname}${file}`));
+
+  //testing
+  var fileTest = path.join(dir,`${uuid}.${format}`);
+  var type = mime[path.extname(fileTest).slice(1)] || 'text/plain';
+  console.log(path.extname(fileTest).slice(1) || 'text/plain');
+  console.log(type);
+  var s = fs.createReadStream(fileTest);
+  s.on('open', function () {
+      res.set('Content-Type', type);
+      s.pipe(res);
+  });
+
+  // // Save the image uuid and expired date in DB
+  // const Image = mongoose.model("Image");
+  // // Date.Now plus 15days (15*24*60*60*1000)
+  // var tmpImage = await Image.create({
+  //   name: uuid,
+  //   expireDate: Date.now() + 15 * 24 * 60 * 60 * 1000,
+  // });
+  // console.log(`imagen creada ${tmpImage} `);
+  
+
   // respond with the image url
   // considerations
   // Temporary image route = images/tmp
@@ -36,6 +90,7 @@ app.post("/image", async function (req, res) {
 app.get("/image/:imageName", async function (req, res) {
   const Image = mongoose.model("Image");
   const tmpImage = await Image.findOne({ name: req.params.imageName });
+  console.log(tmpImage);
   if (tmpImage) {
     //
     res.status(200).send({
@@ -58,5 +113,11 @@ app.get("/hosting/:imageName", async function (req, res) {
     res.redirect("http://www.google.com");
   }
 });
+
+
+
+
+
+
 
 app.listen(3000);
